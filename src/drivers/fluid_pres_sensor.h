2f32c55fb00d6b5628c5d644caadda7d354d7ef2
Jeffrey den Drijver <jeffrey@stud-io.com>
2022-07-23 14:36:55 +0200

    Fix fluid pressure float casting

--- src/drivers/fluid_pres_sensor.h
+++ src/drivers/fluid_pres_sensor.h
@@ -3,16 +3,23 @@
 
 #include <micro_ros_arduino.h>
 
-#define PRES_SENS_PIN 13
+#define PRES_SENS_PIN 34
 #define PRES_SENS_MIN_PASCALS 0
 #define PRES_SENS_MAX_PASCALS 1200000
+// #define PRES_SENS_MAX_PASCALS 792000 // Adjusted for 3.3v
 
 float fluid_pres_pascals;
 
 void fluid_pres_update(){
     int fluid_pres_reading = analogRead(PRES_SENS_PIN);
-    float fluid_pres_rel = fluid_pres_reading / 4096;
+    float fluid_pres_rel = (float)fluid_pres_reading / (float)4096;
     fluid_pres_pascals = (fluid_pres_rel * PRES_SENS_MAX_PASCALS - PRES_SENS_MIN_PASCALS) + PRES_SENS_MIN_PASCALS;
+
+    // Serial.print(fluid_pres_reading);
+    // Serial.print(",");
+    // Serial.print(fluid_pres_rel);
+    // Serial.print(",");
+    // Serial.println(fluid_pres_pascals);
 }
 
 #endif
